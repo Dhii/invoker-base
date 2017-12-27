b927c6bd9be04912db5a6b7825cb2465b5c8d271
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-27 23:09:47 +0100

    Added `CreateInvocationFailureExceptionCapableTrait`

---
+++ src/Exception/CreateInvocationFailureExceptionCapableTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Dhii\Invocation\Exception;
+
+use Dhii\Invocation\CommandInvokerInterface;
+use Exception as RootException;
+use Dhii\Util\String\StringableInterface as Stringable;
+
+trait CreateInvocationFailureExceptionCapableTrait
+{
+    /**
+     * Creates a new Invocation Failure Exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param null|string|Stringable       $message  The error message, if any.
+     * @param null|int|string|Stringable   $code     The error code, if any.
+     * @param RootException|null           $previous The inner exception, if any.
+     * @param CommandInvokerInterface|null $invoker  The problematic invoker, if any.
+     * @param string|Stringable|null       $command  The command that failed, if any.
+     * @param array|null                   $args     The command arguments, if any.
+     *
+     * @return CommandInvokerException The new exception.
+     */
+    protected function _createInvocationFailureException(
+            $message = null,
+            $code = null,
+            RootException $previous = null,
+            CommandInvokerInterface $invoker = null,
+            $command = null,
+            $args = null
+    ) {
+        return new InvocationFailureException($message, $code, $previous, $invoker, $command, $args);
+    }
+}
