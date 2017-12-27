a09a1259e2d10d0dc4dd18abb560973d0d1876a4
Anton Ukhanev <xedin.unknown@gmail.com>
2017-12-27 23:09:08 +0100

    Added `CreateCommandInvokerExceptionCapableTrait`

---
+++ src/Exception/CreateCommandInvokerExceptionCapableTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dhii\Invocation\Exception;
+
+use Dhii\Invocation\CommandInvokerInterface;
+use Exception as RootException;
+use Dhii\Util\String\StringableInterface as Stringable;
+
+trait CreateCommandInvokerExceptionCapableTrait
+{
+    /**
+     * Creates a new Command Invoker Exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param null|string|Stringable       $message  The error message, if any.
+     * @param null|int|string|Stringable   $code     The error code, if any.
+     * @param RootException|null           $previous The inner exception, if any.
+     * @param CommandInvokerInterface|null $invoker  The problematic invoker, if any.
+     *
+     * @return CommandInvokerException The new exception.
+     */
+    protected function _createCommandInvokerException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        CommandInvokerInterface $invoker = null
+    ) {
+        return new CommandInvokerException($message, $code, $previous, $invoker);
+    }
+}
